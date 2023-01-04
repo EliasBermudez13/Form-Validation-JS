39b38118880b4c519fa04ea5fcd8d2cf62cfae6f
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-01-04 15:30:08 +0000

    Validation on forms for empty fields

--- src/app.js
+++ src/app.js
@@ -8,4 +8,35 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  var form = document.getElementById("form");
+
+  form.addEventListener("submit", prueba);
+  let allValues = document.querySelectorAll("input[type=text], textarea");
+
+  let alertError = document.getElementById("alertError");
+  let state = document.getElementById("inputState");
+
+  function prueba(e) {
+    e.preventDefault();
+    for (var values of allValues) {
+      if (values.value === "") {
+        values.style.background = "pink";
+        alertError.classList.remove("invisible");
+      } else {
+        // console.log("All good, form send");
+        values.style.background = "#fff";
+        // alertError.classList.add("invisible");
+      }
+    }
+    let elementoActivo = document.querySelector(
+      'input[name="inlineRadioOptions"]:checked'
+    );
+    if (!elementoActivo) {
+      console.log("El campo credit card esta vacio");
+      alertError.classList.remove("invisible");
+    }
+    if (state.value === "Pick a state") {
+      console.log("El campo state está vacio");
+    }
+  }
 };
